f0d50d1793627e660cedb247b6c1f3b500e9443c
Kyle <petersen_kyle2000@yahoo.com>
2022-01-08 15:34:44 -0800

    find then update

--- myApp.js
+++ myApp.js
@@ -77,13 +77,13 @@ const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
   Person.findOneAndUpdate(
     { name: personName },
+    { age: ageToSet },
+    { new: true },
     (err, data) => {
       if (err) return console.log(err);
-      data.age = ageToSet;
-    },
-    { new: true }
+      done(null, data);
+    }
   );
-  done(null, data);
 };
 
 const removeById = (personId, done) => {
