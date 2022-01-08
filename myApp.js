41bd7e0565fdfa525c6e53e621072b942bd2b11a
Kyle <petersen_kyle2000@yahoo.com>
2022-01-08 13:10:02 -0800

    added schema

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-const Schema = mongoose.Schema;
+
 const personSchema = new Schema({
   name: { type: String, required: true },
   age: Number,
